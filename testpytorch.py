e004adce53847a20dbbf8830d1a6507f72e10f12
kitsune27 <soo_jw@yahoo.com>
2020-08-26 20:52:10 +0800

    Initial commit

---
+++ testpytorch.py
@@ -0,0 +1,18 @@
+import torch
+import torchvision
+import torch.utils.data
+import matplotlib.pyplot as plt
+from torchvision import transforms, datasets
+
+
+train = datasets.MNIST("", train=True, download=True, transform=transforms.Compose([transforms.ToTensor()]))
+test = datasets.MNIST("", train=False, download=True, transform=transforms.Compose([transforms.ToTensor()]))
+trainset = torch.utils.data.DataLoader(train, batch_size=10, shuffle=True)
+testset = torch.utils.data.DataLoader(test, batch_size=10, shuffle=True)
+total = 0
+counter_dict = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
+for a in trainset:
+    print(a)
+    break
+
+# print(counter_dict)
